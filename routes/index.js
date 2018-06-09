965279650eba8c25f5c1827fe24f3c60616f555f
Anderson Tchuy <andersontchuy@hotmail.com>
2018-06-09 01:13:24 -0300

    Update index style

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'mbapp' });
+  res.render('index', { title: 'MCoinapp' });
 });
 
 module.exports = router;
