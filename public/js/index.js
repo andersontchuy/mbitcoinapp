4a948acc87bcbb146ac5fa8e6e303d18b385824f
Anderson Tchuy <andersontchuy@hotmail.com>
2018-06-21 00:08:41 -0300

    Add bitcoin ticker in header

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,53 @@
-window.onload = function() {
+$(function() {
+    const path = "https://www.mercadobitcoin.net/api";
+
+    loadBitcoin();
+
     function elementId(id) {
         return document.getElementById(id);
     }
+ 
+    function loadBitcoin() {
+      const tickerLast = elementId('ticker-last');
+      const tickerHigh = elementId('ticker-high');
+      const tickerLow = elementId('ticker-low');
+      const tickerVol = elementId('ticker-vol');
 
-    function showTalk(event) {
-        const talk = event.target.innerHTML;
-        elementId('txt').innerHTML = `Negociações ${talk}`;
+      tickerLast.innerHTML = '-';
+      tickerHigh.innerHTML = '-';
+      tickerLow.innerHTML = '-';
+      tickerVol.innerHTML = '-';
+
+      $.getJSON(`${path}/btc/ticker/`, function(data) {
+          const last = parseFloat(data.ticker.last).toFixed(2);
+          const high = parseFloat(data.ticker.high).toFixed(2);
+          const low = parseFloat(data.ticker.low).toFixed(2);
+          const vol = parseFloat(data.ticker.vol).toFixed(3);
+
+          tickerLast.innerHTML = `R$ ${last}`;
+          tickerHigh.innerHTML = `R$ ${high}`;
+          tickerLow.innerHTML = `R$ ${low}`;
+          tickerVol.innerHTML = `${vol} BTC`; 
+
+          console.log(data);
+        });
     }
 
-    elementId('btc').onclick = showTalk;
-    elementId('ltc').onclick = showTalk;
-    elementId('bch').onclick = showTalk;
-}
+    function showTicker(event) {
+        const coin = event.target;
+        elementId('txt').innerHTML = `Negociações ${coin.innerHTML}`;
+
+        $.getJSON(`${path}/${coin.id}/ticker/`).done(function(data) {  
+          console.log(data);
+        });
+
+        console.log(`${path}/${coin.id}/ticker/`);
+        
+    }
+
+    elementId('btc').onclick = showTicker;
+    elementId('ltc').onclick = showTicker;
+    elementId('bch').onclick = showTicker;
+
+    setInterval(loadBitcoin, 20000);
+});
