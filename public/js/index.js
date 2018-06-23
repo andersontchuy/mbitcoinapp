284709ccbb9e2f9a4a3021dfa52ae92d3b6c1859
Anderson Tchuy <andersontchuy@hotmail.com>
2018-06-23 20:20:38 -0300

    Refactoring functions loadCoin and showTicker

--- public/js/index.js
+++ public/js/index.js
@@ -1,53 +1,53 @@
 $(function() {
     const path = "https://www.mercadobitcoin.net/api";
+    const selected = { id: 'btc' };
 
-    loadBitcoin();
+    loadCoin(selected);
 
     function elementId(id) {
-        return document.getElementById(id);
+      return document.getElementById(id);
     }
  
-    function loadBitcoin() {
-      const tickerLast = elementId('ticker-last');
-      const tickerHigh = elementId('ticker-high');
-      const tickerLow = elementId('ticker-low');
-      const tickerVol = elementId('ticker-vol');
-
-      tickerLast.innerHTML = '-';
-      tickerHigh.innerHTML = '-';
-      tickerLow.innerHTML = '-';
-      tickerVol.innerHTML = '-';
-
-      $.getJSON(`${path}/btc/ticker/`, function(data) {
-          const last = parseFloat(data.ticker.last).toFixed(2);
-          const high = parseFloat(data.ticker.high).toFixed(2);
-          const low = parseFloat(data.ticker.low).toFixed(2);
-          const vol = parseFloat(data.ticker.vol).toFixed(3);
-
-          tickerLast.innerHTML = `R$ ${last}`;
-          tickerHigh.innerHTML = `R$ ${high}`;
-          tickerLow.innerHTML = `R$ ${low}`;
-          tickerVol.innerHTML = `${vol} BTC`; 
-
-          console.log(data);
-        });
+    function loadCoin(value) {
+      showTicker(value);
     }
 
-    function showTicker(event) {
-        const coin = event.target;
-        elementId('txt').innerHTML = `Negociações ${coin.innerHTML}`;
+    function showTicker(value) {
+      const coin = value.target === undefined 
+        ? selected : value.target;
+      const field = { 
+        last: elementId('ticker-last'),
+        high: elementId('ticker-high'),
+        low: elementId('ticker-low'),
+        vol: elementId('ticker-vol')
+      }
 
-        $.getJSON(`${path}/${coin.id}/ticker/`).done(function(data) {  
-          console.log(data);
-        });
+      field.last.innerHTML = '-';
+      field.high.innerHTML = '-';
+      field.low.innerHTML = '-';
+      field.vol.innerHTML = '-';
 
-        console.log(`${path}/${coin.id}/ticker/`);
+      $.getJSON(`${path}/${coin.id}/ticker/`).done(function(data) {  
+        field.last.innerHTML = `R$ ${parseFloat(data.ticker.last)
+          .toFixed(2).replace('.', ',')}`;
+        field.high.innerHTML = `R$ ${parseFloat(data.ticker.high)
+          .toFixed(2).replace('.', ',')}`;
+        field.low.innerHTML = `R$ ${parseFloat(data.ticker.low)
+          .toFixed(2).replace('.', ',')}`;
+        field.vol.innerHTML = `${parseFloat(data.ticker.vol)
+          .toFixed(3).replace('.', ',')} ${coin.id.toUpperCase()}`; 
+
+        console.log(data);
+      });
+      console.log('selectedCoin: ', selected);
+      selected.id = coin.id;
+      console.log(`${path}/${coin.id}/ticker/`);
         
     }
 
-    elementId('btc').onclick = showTicker;
-    elementId('ltc').onclick = showTicker;
-    elementId('bch').onclick = showTicker;
+    elementId('btc').onclick = loadCoin;
+    elementId('ltc').onclick = loadCoin;
+    elementId('bch').onclick = loadCoin;
 
-    setInterval(loadBitcoin, 20000);
+    setInterval(showTicker, 20000, selected);
 });
